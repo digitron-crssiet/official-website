903a8a2c9dc4193e2498cd9a23c668e2b950c8d4
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-06 22:12:27 +0530

    route homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <h1>
-      Hello From Digitron
-    </h1>
+    <>
+      <Routes>
+        <Route path="/" element={<>Hello Digitron</>}/>
+      </Routes>
+    </>
   )
 }
 
